126d19402d5305ec622d394f9d8f1922dc6afccf
tukaram <tukarammbaragukar@gmail.com>
2024-12-28 19:34:57 +0530

    first commit

---
+++ if else condition/max.py
@@ -0,0 +1,19 @@
+n1=int(input("Enter a number "))
+n2=int(input("Enter a number "))
+if n1>n2:
+    print(f'{n1} is greter than {n2}')
+else:
+     print(f'{n1} is less than {n2}')
+
+     '''
+     Enter a number 6
+Enter a number 4
+6 is greter than 4
+PS E:\PYTHON> & C:/Users/LENOVO/AppData/Local/Microsoft/WindowsApps/python3.9.exe "e:/PYTHON/conditional statement/if 
+else condition/max.py"
+Enter a number 3
+Enter a number 7
+3 is less than 7
+PS E:\PYTHON> 
+     
+     '''
---
+++ if else condition/palindrome.py
@@ -0,0 +1,26 @@
+
+s=input("Enter a word ")
+if s[::-1]==s:
+    print(f'{s} is a pallindrome')
+else:
+    print(f'{s} is not a pallindrome')
+
+'''Enter a wordmalayalam
+malayalam is a pallindrome
+PS E:\PYTHON> & C:/Users/LENOVO/AppData/Local/Microsoft/WindowsApps/python3.9.exe "e:/PYTHON/conditional statement/if 
+else condition/palindrome.py"
+Enter a wordnitin
+nitin is a pallindrome
+PS E:\PYTHON> & C:/Users/LENOVO/AppData/Local/Microsoft/WindowsApps/python3.9.exe "e:/PYTHON/conditional statement/if 
+else condition/palindrome.py"
+Enter a wordpip
+pip is a pallindrome
+PS E:\PYTHON> & C:/Users/LENOVO/AppData/Local/Microsoft/WindowsApps/python3.9.exe "e:/PYTHON/conditional statement/if 
+else condition/palindrome.py"
+Enter a word hiih
+hiih is a pallindrome
+PS E:\PYTHON> & C:/Users/LENOVO/AppData/Local/Microsoft/WindowsApps/python3.9.exe "e:/PYTHON/conditional statement/if 
+else condition/palindrome.py"
+Enter a word hello
+hello is not a pallindrome
+'''
